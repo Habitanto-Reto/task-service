e8aae5058ea67af0297e84227eec0a6629917a53
Santiago Gaona <sgaonacarvajal@outlook.com>
2024-01-21 18:52:20 -0500

    🐛fix: model request create task (#12)
    * 🐛fix: model request create task (#11)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+### 0.0.7 (2024-01-21)
+
+### 0.0.6 (2024-01-21)
+
 ### 0.0.5 (2024-01-21)
 
 ### 0.0.4 (2024-01-21)
--- src/domain/entities/ITask.ts
+++ src/domain/entities/ITask.ts
@@ -16,9 +16,9 @@ const TaskSchema: Schema = new Schema({
     title: { type: String, required: true },
     creatorUserId: { type: String, required: true },
     assigneeUserId: { type: String, required: true },
-    isCompleted: { type: Boolean, required: true },
-    createdDate: { type: Date, required: true },
-    completedDate: { type: Date, required: true }
+    isCompleted: { type: Boolean, required: false },
+    createdDate: { type: Date, default: Date.now  },
+    completedDate: { type: Date, required: false }
 });
 
 export default mongoose.model<ITask, mongoose.Model<ITask>>('Task', TaskSchema);
